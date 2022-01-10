b81467c7e2cf895c2753242a30d4f224014018db
Ali Darwish <ali.2512.2206@gmail.com>
2022-01-10 21:33:34 +0200

    Modified app.js

--- app.js
+++ app.js
@@ -1,2 +1,61 @@
 console.log("Hello World");
 alert("Hello World");
+
+var number = 5; //This is an inline comment!
+
+/*This is a 
+multiline comment */
+
+number = 9;
+
+/*Data types:
+undefined, null(nothing), boolean (True or False), string, symbol, number, and object.
+*/
+
+var myName = "Ali Darwish"; // Global
+
+myName = 8;
+
+let ourName = "Darwish"; // Only used within the scope
+
+const pi = 3.14; // It cannot ever be modified
+
+// Storing Values with the Assignment Operator
+
+var b = 2;
+
+a = 7;
+
+b = a;
+
+console.log(a);
+
+var a = 5;
+
+var b = 10;
+
+var c = "I am a";
+
+a = a + 1;
+
+b = b + 5;
+
+c = c + "String";
+
+var Study;
+
+stuDy = 10; // Case sensitive.
+
+var studyMeNow;
+
+studyMeNow = 10;
+
+a = a + 12;
+a += 12;
+
+b = b - 2;
+b -= 2;
+
+// Works for all operators + - * /
+
+// Declare String Variables
